8e1ba7a28187aab2197ffd5f66817d43dc64b6d2
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2017-12-12 17:31:32 +0100

    SQL - Moved under Assets + Added Comment Table

--- web/assets/sql/silexblog_bdd.sql
+++ web/assets/sql/silexblog_bdd.sql
@@ -31,10 +31,28 @@ COLLATE utf8_bin;
 
 
 /*==============================================================*/
+/* TABLE CREATION : pwsb_Comment                                */
+/*==============================================================*/
+CREATE TABLE pwsb_Comment
+(
+    id          INT NOT NULL AUTO_INCREMENT,
+    id_post     INT NOT NULL,
+    date        DATETIME NOT NULL,
+    content     VARCHAR(240) NOT NULL,
+    
+    PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8
+COLLATE utf8_bin;
+
+
+/*==============================================================*/
 /* ADDING CONSTRAINTS (FOREIGN KEYS)                            */
 /*==============================================================*/
-#alter TABLE qcmaster_QCM ADD CONSTRAINT FK_QCMTeacher foreign KEY (id_teacher)
-#      references qcmaster_Teacher (id) on delete restrict on update restrict;
+
+ALTER TABLE pwsb_Comment ADD CONSTRAINT FK_PostComments foreign KEY (id_post)
+      REFERENCES pwsb_Blogpost(id) on delete restrict on update restrict;
 	  
 #alter TABLE qcmaster_Question ADD CONSTRAINT FK_QuestionQCM foreign KEY (id_QCM)
 #      references qcmaster_QCM (id) on delete restrict on update restrict;
@@ -47,7 +65,7 @@ COLLATE utf8_bin;
 /* INSERTING DEFAULT/TEST VALUES                                */
 /*==============================================================*/
 
-/* Let's create three Blogposts here */
+/* Let's create two Blogposts here */
 INSERT INTO pwsb_Blogpost (date, title, content, image) VALUES
 ('2016-01-01 18:05:00','Ouverture du Blog !', "
 Bienvenue à tous, le Blog est désormais ouvert ! N'hésitez pas à commenter mes articles :D
@@ -60,6 +78,13 @@ Article sur les licornes et leur existence prouvée
 Aliquam pharetra tellus purus, in varius diam aliquam non. Phasellus tempus tincidunt dignissim. Aliquam at erat a sem malesuada egestas. Integer eros nulla, porttitor vitae pretium vel, accumsan vel nisl. Morbi convallis dui in diam eleifend consectetur. Duis sit amet erat nibh. Sed et neque vel mauris aliquam ornare ut nec elit. In venenatis sed massa nec tincidunt. Pellentesque id nulla eget massa dignissim bibendum pharetra vitae mauris. Morbi viverra malesuada nisi, in porta nisi porta in. Cras lectus orci, luctus eu fringilla ac, pulvinar sed lectus. Donec lobortis imperdiet justo, eget semper justo pellentesque nec. Sed accumsan vitae risus nec fringilla. Maecenas ut orci ac turpis ultrices posuere. Integer augue sapien, ornare at fermentum a, rhoncus ut leo.
 Vestibulum sagittis orci sed lectus consectetur gravida. Aliquam pharetra faucibus dolor. Suspendisse vulputate, nulla non egestas convallis, nibh ipsum congue nisl, ac pulvinar sapien eros vel dolor. Pellentesque bibendum malesuada pellentesque. Vivamus vitae eleifend lectus. Integer sed nulla non tellus dapibus pulvinar. Nam id leo neque. Cras aliquam sit amet arcu id elementum. Duis vitae erat quis ante aliquam pulvinar. Pellentesque in quam mauris. Morbi feugiat condimentum blandit. Praesent in tempus diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis pretium porta risus id pretium. Phasellus quis viverra massa, at volutpat sem. Nulla ut est sed quam laoreet placerat vitae sit amet tellus.
 ", "");
+
+/* Let's create four Comments here */
+INSERT INTO pwsb_Comment (id_post, date, content) VALUES
+(1,'2016-01-01 00:32:02','Génial, un nouveau blog !'),
+(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(2,'2017-12-24 18:30:20','J\'en ai vu une hier :O :D');
 
 /* Let's create two Teachers here */
 #INSERT INTO qcmaster_Teacher (email, firstname, lastname, password) VALUES

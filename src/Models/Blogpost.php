52b0f9a4ab61181e4cab083e2c2030fbc8410434
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-12-25 20:52:02 +0100

    MVC Controller - Blogpost : Removed Comments

--- src/Models/Blogpost.php
+++ src/Models/Blogpost.php
@@ -33,7 +33,7 @@ class Blogpost {
     protected $content;
         
     /**
-     * @var string : Link to the image displayed on the Blogpost thumbnail (can be empty)
+     * @var string : Path to the image displayed on the Blogpost thumbnail (can be empty)
      */
     protected $image;
         
@@ -66,66 +66,7 @@ class Blogpost {
         $this->comments = new ArrayCollection();
         
     }
-    
-//    /**
-//     * QCM's Constructor : Empty Constructor
-//     * @param null : This function needs no parameters
-//     * @return null : This function returns nothing
-//     */
-//    public function __construct(){}
-//    /**
-//     * QCM's Constructor : for the qcm create with de DB
-//     * @param null : This function needs no parameters
-//     * @return null : This function returns nothing
-//     */
-//    public static function constructFromDB($id){
-//        
-//        $db = new Database();
-//        //request for qcm infos
-//        $db->query('SELECT * FROM '. TABLE_QCM .' WHERE id = :id');
-//        $db->bind(':id', $id);
-//        $rows = $db->single();
-//        
-//        //affect results in Attributes
-//        
-//        $QCM=new QCM();
-//        
-//        $QCM->id=$rows['id'];
-//        $QCM->teacher_id=$rows['id_teacher'];
-//        $QCM->title=$rows['title'];
-//        $QCM->topic=$rows['topic'];
-//        $QCM->link=$rows['link'];
-//        
-//        //request for questions
-//        $db->query('SELECT * FROM '. TABLE_QUESTION .' WHERE id_QCM = :id');
-//        $db->bind(':id', $id);
-//        $rows = $db->resultset();
-//        foreach($rows as $row){
-//            $QCM->questions[]= new Question($row['id'],$row['title']);
-//            
-//            //request for answers
-//            $db->query('SELECT * FROM '. TABLE_ANSWER .' WHERE id_Question = :id');
-//            $db->bind(':id', $row['id']);
-//            $answs = $db->resultset();
-//            foreach($answs as $answ){
-//                $QCM->questions[sizeof($QCM->questions)-1]->addAnswer(new Answer($answ['id_question'],$answ['correct'],$answ['proposition']));
-//                
-//            }
-//        }
-//        
-//        return $QCM;
-//        
-//    }
-//    /**
-//     * QCM's Constructor : for the qcm create from scratch
-//     * @param null : This function needs no parameters
-//     * @return null : This function returns nothing
-//     */
-//    public static function constructFromScratch(){
-//        $this->title="";
-//        $this->topic="";
-//    }
-    
+        
     /* ----- -----  ----- ----- Accessor(s) ----- -----  ----- ----- */
     
     /**
@@ -236,60 +177,6 @@ class Blogpost {
         return $nb_com;
     }
     
-//    /**
-//     * Accessor 'getID' : Returns the id of that QCM
-//     * @param null : This function needs no parameters
-//     * @return int
-//     */    
-//    function getID() {
-//        return $this->id;
-//    }
-//    
-//    /**
-//     * Accessor 'getTeacherID' : Returns the id of the Owner of that QCM
-//     * @param null : This function needs no parameters
-//     * @return int
-//     */    
-//    function getTeacherID() {
-//        return $this->teacher_id;
-//    }
-//    
-//    /**
-//     * Accessor 'getTitle' : Returns the title of that QCM
-//     * @param null : This function needs no parameters
-//     * @return string
-//     */
-//    function getTitle() {
-//        return $this->title;
-//    }
-//    
-//    /**
-//     * Accessor 'getTopic' : Returns the topic of that QCM
-//     * @param null : This function needs no parameters
-//     * @return string
-//     */
-//    function getTopic() {
-//        return $this->topic;
-//    }
-//    
-//    /**
-//     * Accessor 'getLink' : Returns the link of that QCM
-//     * @param null : This function needs no parameters
-//     * @return string
-//     */
-//    function getLink() {
-//        return $this->link;
-//    }
-//    
-//    /**
-//     * Accessor 'getQuestions' : Returns the array containing all questions of that QCM
-//     * @param null : This function needs no parameters
-//     * @return array<Question>
-//     */
-//    function getQuestions() {
-//        return $this->questions;
-//    }
-    
     /* ----- -----  ----- ----- Mutator(s) ----- -----  ----- ----- */
     
     /**
@@ -302,15 +189,6 @@ class Blogpost {
         $this->date = date('Y-m-d H:i:s');
         
     }
-    
-//    /**
-//     * Mutator 'setID' : Modify the id of that QCM
-//     * @param int $id : The new id of that QCM
-//     * @return null : This function returns nothing
-//     */
-//    function setID($id) {
-//        $this->id = $id;
-//    }
 
     /**
      * Mutator 'setTitle' : Modify the title of that post

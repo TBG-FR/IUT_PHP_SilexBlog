fef9509c614ed5e2e8bb3fc26f921fc642ce0e5e
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-12-28 00:38:52 +0100

    SQL Script - Added User table & Foreign key (+related)

--- web/assets/sql/silexblog_bdd.sql
+++ web/assets/sql/silexblog_bdd.sql
@@ -9,7 +9,7 @@
 /*==============================================================*/
 DROP TABLE IF EXISTS pwsb_Comment;
 DROP TABLE IF EXISTS pwsb_Blogpost;
-#DROP TABLE IF EXISTS pwsb_User;
+DROP TABLE IF EXISTS pwsb_User;
 
 
 /*==============================================================*/
@@ -37,8 +37,26 @@ CREATE TABLE pwsb_Comment
 (
     id          INT NOT NULL AUTO_INCREMENT,
     id_post     INT NOT NULL,
+    id_user     INT,
     date        DATETIME NOT NULL,
     content     VARCHAR(240) NOT NULL,
+    
+    PRIMARY KEY (id)
+)
+ENGINE=InnoDB
+CHARACTER SET utf8
+COLLATE utf8_bin;
+
+
+/*==============================================================*/
+/* TABLE CREATION : pwsb_User                                   */
+/*==============================================================*/
+CREATE TABLE pwsb_User
+(
+    id          INT NOT NULL AUTO_INCREMENT,
+    username    VARCHAR(150) NOT NULL,
+    password    VARCHAR(150) NOT NULL,
+    admin       BOOL NOT NULL DEFAULT 0,
     
     PRIMARY KEY (id)
 )
@@ -51,8 +69,17 @@ COLLATE utf8_bin;
 /* ADDING CONSTRAINTS (FOREIGN KEYS)                            */
 /*==============================================================*/
 
-ALTER TABLE pwsb_Comment ADD CONSTRAINT FK_PostComments foreign KEY (id_post)
-      REFERENCES pwsb_Blogpost(id) on delete cascade on update restrict;
+ALTER TABLE pwsb_Comment
+    ADD CONSTRAINT FK_PostComments foreign KEY (id_post) REFERENCES pwsb_Blogpost(id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT;
+        -- => If a Post is deleted, his comments too (but not on update)
+
+ALTER TABLE pwsb_Comment
+    ADD CONSTRAINT FK_UserComments foreign KEY (id_user) REFERENCES pwsb_User(id) 
+        ON DELETE SET NULL
+        ON UPDATE RESTRICT;
+        -- => If a User is deleted, his comments are now owned by 'NULL' (but not on update)
 	  
 #alter TABLE qcmaster_Question ADD CONSTRAINT FK_QuestionQCM foreign KEY (id_QCM)
 #      references qcmaster_QCM (id) on delete restrict on update restrict;
@@ -79,20 +106,27 @@ Aliquam pharetra tellus purus, in varius diam aliquam non. Phasellus tempus tinc
 Vestibulum sagittis orci sed lectus consectetur gravida. Aliquam pharetra faucibus dolor. Suspendisse vulputate, nulla non egestas convallis, nibh ipsum congue nisl, ac pulvinar sapien eros vel dolor. Pellentesque bibendum malesuada pellentesque. Vivamus vitae eleifend lectus. Integer sed nulla non tellus dapibus pulvinar. Nam id leo neque. Cras aliquam sit amet arcu id elementum. Duis vitae erat quis ante aliquam pulvinar. Pellentesque in quam mauris. Morbi feugiat condimentum blandit. Praesent in tempus diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis pretium porta risus id pretium. Phasellus quis viverra massa, at volutpat sem. Nulla ut est sed quam laoreet placerat vitae sit amet tellus.
 ", "");
 
+/* Let's create four Users here */
+INSERT INTO pwsb_User (username, password, admin) VALUES
+('Admin','Admin',1),
+('TBG','azerty',1),
+('Jean','11',0),
+('Hubert','11',0);
+
 /* Let's create four Comments here */
-INSERT INTO pwsb_Comment (id_post, date, content) VALUES
-(1,'2016-01-01 00:32:02','Génial, un nouveau blog !'),
-(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
-(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
-(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
-(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
-(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
-(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
-(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
-(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
-(1,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
-(2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
-(2,'2017-12-24 18:30:20','J\'en ai vu une hier :O :D');
+INSERT INTO pwsb_Comment (id_post, id_user, date, content) VALUES
+(1,1,'2016-01-01 00:32:02','Génial, un nouveau blog !'),
+(1,2,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,3,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(1,4,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,3,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(1,2,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,1,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(1,4,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,2,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(1,3,'2016-02-01 14:24:40','Encore un blog voué à l\'échec...'),
+(2,1,'2017-12-25 00:30:06','Je n\'y crois, Monsieur, vous êtes un Charlatan ! Joyeux Noël tout de même.'),
+(2,4,'2017-12-24 18:30:20','J\'en ai vu une hier :O :D');
 
 /* Let's create two Teachers here */
 #INSERT INTO qcmaster_Teacher (email, firstname, lastname, password) VALUES
